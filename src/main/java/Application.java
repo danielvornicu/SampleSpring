e6c828c851c13f6aa6f33a2292ca39512844cb80
VORNICU Daniel <d.vornicu@anfh.fr>
2018-06-26 10:18:05 +0200

    Initial commit

---
+++ src/main/java/Application.java
@@ -0,0 +1,21 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * La classe principale
+ * <p>
+ * Date: 25/06/2018
+ *
+ * @author d.vornicu
+ * @version 1.0 $Revision$ $Date$
+ */
+
+public class Application {
+
+    private static Logger LOGGER = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        LOGGER.debug("Startup");
+
+    }
+}
