f05aac7fba07aea8073563927b868f29cc686f41
danielvornicu <vdany2003@yahoo.com>
2018-06-27 16:56:10 +0200

    Initial commit

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,8 @@
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import tech.dev.modele.Client;
 
 /**
  * La classe principale
@@ -16,6 +19,26 @@ public class Application {
 
     public static void main(String[] args) {
         LOGGER.debug("Startup");
+        //
+        LOGGER.debug("Init Spring context...");
+        AbstractApplicationContext context = new ClassPathXmlApplicationContext("spring-applicationContext.xml");
+        LOGGER.debug("Get beans from Spring context *.xml file...");
+        Client c1 = context.getBean("c1", Client.class);
+        Client c2 = context.getBean("c2", Client.class);
+        Client c3 = context.getBean("c3", Client.class);
+        Client c4 = context.getBean("c4", Client.class);
 
+        LOGGER.debug("Liste des clients :");
+        //
+        LOGGER.debug(c1.toString());
+        LOGGER.debug(c2.toString());
+        LOGGER.debug(c3.toString());
+        LOGGER.debug(c4.toString());
+
+        LOGGER.debug("Test si adresse est partagé :");
+        System.out.println("Scope pour le bean adresse: " + (c1.getAdresse() == c2.getAdresse() ? "singleton" : "prototype"));
+
+        LOGGER.debug("Close Spring context...");
+        context.close();
     }
 }
