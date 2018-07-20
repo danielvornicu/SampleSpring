ab87eb51c1f3d334b3046922632a37c9d6c7c392
danielvornicu <vdany2003@yahoo.com>
2018-07-20 10:36:53 +0200

    Initial commit

---
+++ src/main/java/tech/dev/service/ClientService.java
@@ -0,0 +1,38 @@
+package tech.dev.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import tech.dev.dao.AdresseJpaDAO;
+import tech.dev.dao.ClientJpaDAO;
+
+/**
+ * Description de la classe
+ * <p>
+ * Date: 19/07/2018
+ *
+ * @author d.vornicu
+ * @version 1.0 $Revision$ $Date$
+ */
+
+@Service
+public class ClientService {
+
+    ClientJpaDAO clientDAO;
+    AdresseJpaDAO adresseDAO;
+
+    @Autowired
+    public ClientService(ClientJpaDAO clientJpaDAO, AdresseJpaDAO adresseDAO) {
+        this.clientDAO  = clientDAO;
+        this.adresseDAO = adresseDAO;
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED, noRollbackFor = NullPointerException.class)
+    public void deleteClientByAdresseId(Long id){
+        //System.out.println(adresseDAO);
+        this.clientDAO.deleteByAdreseId(id);
+        //this.adresseDAO.deleteById(id);
+    }
+
+}
