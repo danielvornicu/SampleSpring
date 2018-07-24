b69bfcbcd6cd1dcb903bbe3e6ca35fbf3edffa89
danielvornicu <vdany2003@yahoo.com>
2018-07-24 15:26:23 +0200

    Initial Commit

---
+++ src/main/java/tech/dev/WebApplication.java
@@ -0,0 +1,41 @@
+package tech.dev;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Description de la classe
+ * <p>
+ * Date: 23/07/2018
+ *
+ * @author d.vornicu
+ * @version 1.0 $Revision$ $Date$
+ */
+
+public class WebApplication {
+    private static Logger LOGGER = LoggerFactory.getLogger(WebApplication.class);
+
+    public static void main(String[] args) throws Exception {
+        Server server = new Server(8090);
+
+        WebAppContext webAppContext = new WebAppContext();
+        webAppContext.setDescriptor("src/main/webapp/WEB-INF/web.xml");
+        webAppContext.setResourceBase("src/main/webapp/");
+        webAppContext.setContextPath("/crm");
+
+        webAppContext.setAttribute("org.eclipse.jetty.server.webapp.ContainerIncludeJarPattern",".*/[^/]*.*\\.jar$");
+
+        //4. Enabling the Annotation based configuration
+        org.eclipse.jetty.webapp.Configuration.ClassList classlist = org.eclipse.jetty.webapp.Configuration.ClassList.setServerDefault(server);
+        classlist.addAfter("org.eclipse.jetty.webapp.FragmentConfiguration", "org.eclipse.jetty.plus.webapp.EnvConfiguration", "org.eclipse.jetty.plus.webapp.PlusConfiguration");
+        classlist.addBefore("org.eclipse.jetty.webapp.JettyWebXmlConfiguration", "org.eclipse.jetty.annotations.AnnotationConfiguration");
+
+        server.setHandler(webAppContext);
+
+        server.start();
+        LOGGER.debug("Web application ready");
+        server.join();
+    }
+}
